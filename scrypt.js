cc72e403785928cbf91281b02cc3edd0f3a1b9b2
Dabidibu <matthewschmtz@gmail.com>
2019-05-29 23:38:38 +0200

    Started develop
    Removed unnecessary files, added necessary.
    
    almost finished first page section (only PC version)

---
+++ scrypt.js
@@ -0,0 +1,19 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', '5.Assets/Particles-js/particlesjs-config.json', function() {
+    console.log('callback - particles.js config loaded');
+});
+
+$('document').ready(function() {
+    var typed = new Typed('#typed', {
+        strings: ["wonderful", "great again", "incredible", "terrific", "fantastic"],
+        stringsElement: "#typed-strings",
+        typeSpeed: 100,
+        backDelay: 4000,
+        backSpeed: 50,
+        shuffle: true,
+        loop: true,
+
+
+
+    });
+});
